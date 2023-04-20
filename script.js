cbce4daa7a5469b948bcb41138d311860328d631
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-04-20 16:20:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+//your code here
+
+let a = null;
+let b = null;
+
+function countClicks(e){
+
+firstclick --> a = img and b= null
+secondclick --> a= img && b=img and if a & b are different, if they match then verify button shud appear and if a===b then sucess else reset
+third click  or more- and double click on the same image-> verify button shud not be rendered 
+}
+img.addeventlistener('click',countClicks) 
+const img1= document.getElementsByClassName('img1');
+const img2= document.getElementsByClassName('img1');
+const img3= document.getElementsByClassName('img1');
+const img1= document.getElementsByClassName('img1');
+const img1= document.getElementsByClassName('img1');
+const img1= document.getElementsByClassName('img1');
